95886c0a7190a6ac2a011bdccf04ae057bdf7e6b
mca <techgiantshck@gmail.com>
2017-07-26 15:56:50 +0530

    test purpose

---
+++ app/src/main/java/com/example/mca/test/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.mca.test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button b1= (Button) findViewById(R.id.button);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "test", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
